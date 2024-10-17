d5f53b07b944b6437b5a0dd272e2d6bd1ce3b68b
Vito Zanotelli <vito.zanotelli@gmail.com>
2024-10-17 15:59:22 +0200

    Move to renv
    Otherwise I did not manage to install the bioconductor dependencies

--- Snakefile
+++ Snakefile
@@ -2,11 +2,35 @@ rule prepare_saint:
     conda:
         "environment.yml"
     input:
-        data_raw_scaffold =  "data/raw/Prepare_SAINT_list_input_file/Proteins Report of merged human files with all 4 conditions (1. Feb, 5. Feb, 18. Feb).txt",
-        data_rename = "data/metadata/SAINT_rename_file.csv"
+        data_raw_scaffold="data/raw/Prepare_SAINT_list_input_file/Proteins Report of merged human files with all 4 conditions (1. Feb, 5. Feb, 18. Feb).txt",
+        data_rename="data/metadata/SAINT_rename_file.csv",
+        renv="restored.Rprofile",
     output:
-        data_saint = "data/interim/SAINT_list_input.csv",
-        data_saint_MTHFR38to65 = "interim/SAINT_list_input_MTHFR38to656_control.csv",
-        crapome = "data/interim/crapome_input.csv"
+        data_saint="data/interim/SAINT_list_input.csv",
+        data_saint_MTHFR38to65="interim/SAINT_list_input_MTHFR38to656_control.csv",
+        crapome="data/interim/crapome_input.csv",
     script:
         "scripts/Script_SAINT_list_input_v1.R"
+
+
+# Use renv
+wildcard_constraints:
+    # Allows renv_dir also to be the empty (=root dir)
+    renv_dir=".*",
+
+
+rule restore_renv:
+    conda:
+        "environment.yml"
+    input:
+        rprofile="{renv_dir}.Rprofile",
+        lock="{renv_dir}renv.lock",
+    output:
+        restored="{renv_dir}restored.Rprofile",
+    log:
+        "logs/renv/{renv_dir}_restore.log",
+    shell:
+        """
+        R -e 'source("{input.rprofile}", chdir=TRUE);renv::restore()'
+        echo 'source("{input.rprofile}", chdir=TRUE)' > {output.restored}
+        """
--- scripts/Script_SAINT_list_input_v1.R
+++ scripts/Script_SAINT_list_input_v1.R
@@ -1,35 +1,39 @@
-#Read the input file ----
+# Read the input file ----
 
-if (exists('snakemake')){
+if (exists("snakemake")) {
   # input
   fn_raw_scaffold <- snakemake@input$data_raw_scaffold
   fn_data_rename <- snakemake@input$data_rename
 
   # output
   fn_saint <- snakemake@output$data_saint
-  fn_saint_MTHFR38to65 <- snakemake@output$data_saint_MTHFR38to65
+  fn_saint_mthfr38to65 <- snakemake@output$data_saint_MTHFR38to65
   fn_crapome <- snakemake@output$crapome
+
+  # renv
+  source(snakemake@input$renv)
 } else {
   fn_raw_scaffold <- "data/raw/Prepare_SAINT_list_input_file/Proteins Report of merged human files with all 4 conditions (1. Feb, 5. Feb, 18. Feb).txt"
   fn_data_rename <- "data/metadata/SAINT_rename_file.csv"
 
   fn_saint <- "data/interim/SAINT_list_input.csv"
-  fn_saint_MTHFR38to65 <- "data/interim/SAINT_list_input_MTHFR38to656_control.csv"
+  fn_saint_mthfr38to65 <- "data/interim/SAINT_list_input_MTHFR38to656_control.csv"
   fn_crapome <- "data/interim/crapome_input.csv"
 }
 
 library(dplyr)
 
-#Import raw csv file. Manually converted from output Scaffold .txt file prior. 
-data_raw_scaffold<- read.csv(fn_raw_scaffold,
-   skip = 1, header = TRUE, check.names = FALSE, sep='\t') #it is just the E1 SYPRO 5x from the original raw data from 220420
-#Import csv file for renaming of Bait and AP-names 
-data_rename<- read.csv(fn_data_rename, header = TRUE, check.names = FALSE) #it is just the E1 SYPRO 5x from the original raw data from 220420
+# Import raw csv file. Manually converted from output Scaffold .txt file prior.
+data_raw_scaffold <- read.csv(fn_raw_scaffold,
+  skip = 1, header = TRUE, check.names = FALSE, sep = "\t"
+) # it is just the E1 SYPRO 5x from the original raw data from 220420
+# Import csv file for renaming of Bait and AP-names
+data_rename <- read.csv(fn_data_rename, header = TRUE, check.names = FALSE) # it is just the E1 SYPRO 5x from the original raw data from 220420
 
-#Modify file ----
+# Modify file ----
 
 # Rename the columns
-data_SAINT_input <- data_raw_scaffold %>%
+data_saint_input <- data_raw_scaffold %>%
   rename(
     `#BaitName_Condition` = `Biological sample category`,
     `#APName` = `Biological sample name`,
@@ -38,51 +42,55 @@ data_SAINT_input <- data_raw_scaffold %>%
   )
 
 # Select only the renamed columns , select suddenly did not work therefore using dplyr::select
-data_SAINT_input <- data_SAINT_input %>% dplyr::select(`#BaitName_Condition`, `#APName`, `#PreyName`, `#SpectralCounts`)
+data_saint_input <- data_saint_input %>% dplyr::select(`#BaitName_Condition`, `#APName`, `#PreyName`, `#SpectralCounts`)
 
 # Remove duplicate rows
-data_SAINT_input <- data_SAINT_input %>% distinct()
+data_saint_input <- data_saint_input %>% distinct()
 
 #  Remove all rows where column "Number of total spectra" == 0
-data_SAINT_input <- data_SAINT_input %>% filter(`#SpectralCounts` != 0)
+data_saint_input <- data_saint_input %>% filter(`#SpectralCounts` != 0)
 
 # Remove all rows with random AP names, e.g. the column "Protein accession number" contains "|"
-data_SAINT_input <- data_SAINT_input %>% filter(!grepl("\\|", `#PreyName`))
+data_saint_input <- data_saint_input %>% filter(!grepl("\\|", `#PreyName`))
 
 # Rename entries in #BaitName_Condition and #APName FindReplace
-#requirements for SAINT input file in list format following https://reprint-apms.org/?q=inputfileformatting
-#Note, the CONTROL should not contain any "_" , the Bait names can not contain any "-"
-bait_name_map <- data_rename[,'#BaitName_Condition_new']
-names(bait_name_map) <- data_rename[,'#BaitName_Condition_old']
-ap_name_map <- data_rename[,'#APName_new']
-names(ap_name_map) <- data_rename[,'#APName_old']
+# requirements for SAINT input file in list format following https://reprint-apms.org/?q=inputfileformatting
+# Note, the CONTROL should not contain any "_" , the Bait names can not contain any "-"
+bait_name_map <- data_rename[, "#BaitName_Condition_new"]
+names(bait_name_map) <- data_rename[, "#BaitName_Condition_old"]
+ap_name_map <- data_rename[, "#APName_new"]
+names(ap_name_map) <- data_rename[, "#APName_old"]
 
-data_SAINT_input <- data_SAINT_input %>%
-  mutate(`#BaitName_Condition` = bait_name_map[`#BaitName_Condition`],
-         `#APName` = ap_name_map[`#APName`])
+data_saint_input <- data_saint_input %>%
+  mutate(
+    `#BaitName_Condition` = bait_name_map[`#BaitName_Condition`],
+    `#APName` = ap_name_map[`#APName`]
+  )
 
 ## Save data ----
 # Save the cleaned data to a new CSV file
-write.csv(data_SAINT_input, fn_saint, row.names = FALSE, quote = FALSE)
+write.csv(data_saint_input, fn_saint, row.names = FALSE, quote = FALSE)
 
-#SAINT input file with MTHFR38to656 as control ----
+# SAINT input file with MTHFR38to656 as control ----
 # Modified input file where Empty vector is removed as a control
-data_SAINT_input_control <- data_SAINT_input %>%
+data_saint_input_control <- data_saint_input %>%
   filter(`#BaitName_Condition` != "CONTROL")
 
 # Re-name MTHFR to CONTROL
-data_SAINT_input_control$`#BaitName_Condition` <- replace(data_SAINT_input_control$`#BaitName_Condition`, 
-                                                          data_SAINT_input_control$`#BaitName_Condition` == "MTHFR_38to656", "CONTROL")
+data_saint_input_control$`#BaitName_Condition` <- replace(
+  data_saint_input_control$`#BaitName_Condition`,
+  data_saint_input_control$`#BaitName_Condition` == "MTHFR_38to656", "CONTROL"
+)
 ## Save data ----
 # Save the cleaned data to a new CSV file
-write.csv(data_SAINT_input_control, fn_saint_MTHFR38to65, row.names = FALSE, quote = FALSE)
+write.csv(data_saint_input_control, fn_saint_mthfr38to65, row.names = FALSE, quote = FALSE)
 
 
-#Crapome list ----
-#For manual retrieval of the Prey appearing in the crapome 
-#Input in "Workflow 1: Query proteins and retrieve profiles"at https://reprint-apms.org/?q=chooseworkflow
-data_crapome<-unique(data_SAINT_input$`#PreyName`)
+# Crapome list ----
+# For manual retrieval of the Prey appearing in the crapome
+# Input in "Workflow 1: Query proteins and retrieve profiles"at https://reprint-apms.org/?q=chooseworkflow
+data_crapome <- unique(data_saint_input$`#PreyName`)
 
 ## Save data ----
 # Save the cleaned data to a new CSV file
-write.table(data_crapome, file=fn_crapome, row.names = FALSE, col.names =FALSE, quote = FALSE)
+write.table(data_crapome, file = fn_crapome, row.names = FALSE, col.names = FALSE, quote = FALSE)
